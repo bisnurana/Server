0c66c9bbcdcf1fbd60bbcc40db51d95c069316fb
Bishnu Rana <bishnu.rana2218@gmail.com>
2017-11-10 19:36:32 +0200

    auth completed

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+//keys for prod env
+module.exports = {
+    // developer console google credentials for oauth google plus authentication using google strategy
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // mlab credentials for connecting with the database
+    mongoURI: process.env.MONGO_URI,
+    //cookie key
+    cookieKey: process.env.COOKIE_KEY
+};
