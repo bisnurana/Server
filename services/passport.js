c855dddc60ef9c0c8b2fc991ad1d39550dd635d1
Bishnu Rana <bishnu.rana2218@gmail.com>
2017-11-10 19:50:05 +0200

    fixeded auth redirect error

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,9 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    // to fix redirect error
+    proxy: true
 }, 
 function(accessToken, refreshToken, profile, done){
     //async queries request that yield promise
